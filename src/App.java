8fe8c273314de84e423af1246798adee72de7b67
niedjo <niedjokuitche@gmail.com>
2025-08-27 18:24:27 +0100

    initializing a java crash course by creating a package

---
+++ src/App.java
@@ -0,0 +1,11 @@
+import com.niedjo.utils.MaClass;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        MaClass obj = new MaClass();
+
+        obj.sayHello();
+        System.out.println("Hello, World!");
+        System.out.println("je suis darren");
+    }
+}
---
+++ src/com/niedjo/utils/MaClass.java
@@ -0,0 +1,7 @@
+package com.niedjo.utils;
+
+public class MaClass {
+    public void sayHello() {
+        System.out.println("Bonjour depuis le package com.niedjo.utils !");
+    }
+}
