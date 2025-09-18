13fca3135390e0efd14b0db7aafc2da59d3aa43e
niedjo <niedjokuitche@gmail.com>
2025-09-18 13:32:12 +0100

    forah + pakages

--- src/App.java
+++ src/App.java
@@ -1,27 +1,15 @@
-import java.util.Scanner;
+import green.red.yellow.RedClass;
 
 public class App {
-    // reading input
     public static void main(String[] args) throws Exception {
-        // to get inputs
-        Scanner scanner = new Scanner(System.in);
+        String[] fruits = { "Apple", "Google", "Facebook", "horizon"};
+ 
+        for (String fruit : fruits) {
+            System.out.println(fruit);
+        }
 
-        // for strings
-        System.out.print("Entrer your full name ");
-        String fullName = scanner.nextLine().trim();
-        System.out.println(fullName);
+        RedClass rd = new RedClass();
 
-        // numbers
-        System.out.print("Give your Age ");
-        byte age1 = scanner.nextByte();
-        System.out.println("Give the age of your wife");
-        byte age2 = scanner.nextByte();
-        System.out.println("Your are " + age1 + " And your wife are " + age2);
-
-        // for strings
-        System.out.print("Entrer your name ");
-        String name = scanner.next();
-        System.out.println(name);
-
+        rd.sayHello(); 
     }
 }
---
+++ src/green/red/yellow/RedClass.java
@@ -0,0 +1,7 @@
+package green.red.yellow;
+
+public class RedClass {
+    public void sayHello() {
+        System.out.println("Hello from RedClass!");
+    }
+}
